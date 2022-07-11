ced8176744e43589e484286b7396448a8d95976b
Angel <47459464+vihangel@users.noreply.github.com>
2022-07-11 00:50:40 -0300

    clear commits

--- cronometro_pet/lib/app/modules/home/timer/timer_controller.dart
+++ cronometro_pet/lib/app/modules/home/timer/timer_controller.dart
@@ -54,35 +54,6 @@ abstract class _TimerControllerBase with Store {
         aux = value - (second * 100);
         mili = aux;
 
-        // if (value.length < 2) {
-        //   mili = "0$value";
-        //   second = "00";
-        //   minute = "00";
-        // } else if (value.length < 3) {
-        //   mili = value.toString();
-        // } else if (value.length < 4) {
-        //   second = '0${value.toString().substring(0, 1)}';
-        //   mili = value.toString().substring(1, 3);
-        // } else if (value.length < 5 && timer.tick < 6000) {
-        //   second = value.toString().substring(0, 2);
-        //   mili = value.toString().substring(2, 4);
-        //   //Fazer uma verificação para os minutos
-
-        // } else if (value.length < 5 && timer.tick >= 6000) {
-        //   minute = '0${((timer.tick~/1000)-5)}';
-        //   second = value.toString().substring(0, 2);
-        //   mili = value.toString().substring(2, 4);
-        //   //Fazer uma verificação para os minutos
-        // } else if (value.length < 6) {
-        //   minute = '0${value.toString().substring(0, 1)}';
-        //   second = value.toString().substring(1, 3);
-        //   mili = value.toString().substring(3, 5);
-        // } else if (value.length < 7) {
-        //   minute = '0${value.toString().substring(0, 2)}';
-        //   second = value.toString().substring(2, 4);
-        //   mili = value.toString().substring(4, 6);
-        // }
-
         if (minute < 10) {
           time = "0$minute";
         } else {
